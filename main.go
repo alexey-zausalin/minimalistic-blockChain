7ea84928d4d67603d94706c48bc8e1b44eab7872
“a.zausalin” <alexeyzausalin@gmail.com>
2024-08-03 17:10:02 +0300

    Test the Transactions

--- main.go
+++ main.go
@@ -9,19 +9,33 @@ import (
 func main() {
 	bc := blockchain.InitBlockChain()
 
-	bc.AddBlock("Block 1")
-	bc.AddBlock("Block 2")
-	bc.AddBlock("Block 3")
+	bc.AddBlock("Block 1", "Alice", []*blockchain.Transaction{
+		{Sender: "Alice", Receiver: "Bob", Amount: 1.5},
+		{Sender: "Alice", Receiver: "Charlie", Amount: 19.5},
+	})
 
-	fmt.Println("BlockChain:")
+	bc.AddBlock("Block 2", "Bob", []*blockchain.Transaction{
+		{Sender: "Bob", Receiver: "Charlie", Amount: 2.3},
+	})
+
 	for _, block := range bc.Blocks {
-		fmt.Printf("\tPrevHash: %x\n", block.PrevHash)
-		fmt.Printf("\tData: %s\n", block.Data)
-		fmt.Printf("\tHash: %x\n", block.Hash)
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash of block: %x\n", block.Hash)
 
 		pow := blockchain.NewProofOfWork(block)
 		fmt.Printf("IsValidPoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 
-		fmt.Println("----------------------------------------")
+		fmt.Println("Transactions:")
+
+		for _, tx := range block.Transactions {
+			fmt.Printf("Sender: %s\n", tx.Sender)
+			fmt.Printf("Receiver: %s\n", tx.Receiver)
+			fmt.Printf("Amount: %f\n", tx.Amount)
+			fmt.Printf("Coinbase: %t\n", tx.Coinbase)
+			fmt.Println()
+		}
+		fmt.Println()
 	}
 }
