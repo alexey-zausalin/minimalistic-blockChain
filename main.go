8a9763615a0465dd7f061338436ba713600876b1
“a.zausalin” <alexeyzausalin@gmail.com>
2024-08-03 13:30:24 +0300

    Test the Blockchain

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"minimalistic-blockChain/blockchain"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	bc := blockchain.InitBlockChain()
+	bc.AddBlock("Block 1")
+	bc.AddBlock("Block 2")
+	bc.AddBlock("Block 3")
+
+	fmt.Println("BlockChain:")
+	for _, block := range bc.Blocks {
+		fmt.Printf("\tPrevHash: %x\n", block.PrevHash)
+		fmt.Printf("\tData: %s\n", block.Data)
+		fmt.Printf("\tHash: %x\n", block.Hash)
+		fmt.Println("----------------------------------------")
+	}
 }
