5aa49d64a63b84d0b6d46d8c102fb7354bd43e79
“a.zausalin” <alexeyzausalin@gmail.com>
2024-08-03 13:45:45 +0300

    Create and Instantiate the Proof of Work Structure

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,1 +1,17 @@
 package blockchain
+
+import "math/big"
+
+const Difficulty = 10
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+
+	return &ProofOfWork{Block: b, Target: target}
+}
