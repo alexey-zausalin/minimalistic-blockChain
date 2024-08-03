ebbf7fe69e987f3059d086da6064f634f90dfc49
“a.zausalin” <alexeyzausalin@gmail.com>
2024-08-03 13:23:36 +0300

    Initialize and Add a Block to the Blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -3,3 +3,15 @@ package blockchain
 type Blockchain struct {
 	Blocks []*Block
 }
+
+func InitBlockChain() *Blockchain {
+	block := Genesis()
+	return &Blockchain{[]*Block{block}}
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.Blocks[len(bc.Blocks)-1]
+	block := CreateBlock(data, prevBlock.Hash)
+
+	bc.Blocks = append(bc.Blocks, block)
+}
