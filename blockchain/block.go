a16ae9915bca9347229765f652e2d3f6b243e54d
“a.zausalin” <alexeyzausalin@gmail.com>
2024-08-03 13:16:42 +0300

    Compute the Hash of a Block

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,7 +1,20 @@
 package blockchain
 
+import (
+	"crypto/md5"
+)
+
 type Block struct {
 	Hash     string
 	Data     string
 	PrevHash string
+}
+
+func (b *Block) ComputeHash() {
+	hasher := md5.New()
+
+	hasher.Write([]byte(b.Data))
+	hasher.Write([]byte(b.PrevHash))
+
+	b.Hash = string(hasher.Sum([]byte{}))
 }
