59fdec8ffda88a164eaf8f1313df8912ffb98732
“a.zausalin” <alexeyzausalin@gmail.com>
2024-08-03 16:51:59 +0300

    Validate the Proof of Work Algorithm

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -63,3 +63,14 @@ func (pow *ProofOfWork) MineBlock() (int, []byte) {
 
 	return nonce, computedHash[:]
 }
+
+func (pow *ProofOfWork) Validate() bool {
+	var intHash big.Int
+
+	computedData := pow.ComputeData(pow.Block.Nonce)
+
+	computedHash := md5.Sum(computedData)
+	intHash.SetBytes(computedHash[:])
+
+	return intHash.Cmp(pow.Target) == -1
+}
