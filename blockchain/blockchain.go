0224bcda6f6e39865c239843423d6a85fbdd3795
“a.zausalin” <alexeyzausalin@gmail.com>
2024-08-03 17:05:27 +0300

    Create the Transaction Structure

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -4,14 +4,29 @@ type Blockchain struct {
 	Blocks []*Block
 }
 
+type Transaction struct {
+	Sender   string
+	Receiver string
+	Amount   float64
+	Coinbase bool
+}
+
 func InitBlockChain() *Blockchain {
 	block := Genesis()
 	return &Blockchain{[]*Block{block}}
 }
 
-func (bc *Blockchain) AddBlock(data string) {
+func (bc *Blockchain) AddBlock(data string, coinbaseRcpt string, transactions []*Transaction) {
 	prevBlock := bc.Blocks[len(bc.Blocks)-1]
-	block := CreateBlock(data, prevBlock.Hash)
 
-	bc.Blocks = append(bc.Blocks, block)
+	coinbaseTransaction := &Transaction{
+		Sender:   "Coinbase",
+		Receiver: coinbaseRcpt,
+		Amount:   10.0,
+		Coinbase: true,
+	}
+
+	newBlock := CreateBlock(data, prevBlock.Hash, append([]*Transaction{coinbaseTransaction}, transactions...))
+
+	bc.Blocks = append(bc.Blocks, newBlock)
 }
